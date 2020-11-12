0f016354abaa9541c5ddc31e16967c42c3368c5c
yuanharnteo <bryson130191@gmail.com>
2020-11-12 22:42:49 +0800

    cleaned

--- Period.java
+++ Period.java
@@ -1,22 +1,20 @@
-import java.text.DateFormat;
 import java.util.*;
 
 public class Period {
     Calendar c = Calendar.getInstance();
     
-    // startPeriod: 25nd December 2020, 12PM 
     private Date startPeriod;
-    // startPeriod: 25nd December 2020, 3PM 
     private Date endPeriod;
 
     public Period(){
+
+        // Default startPeriod: 25th October 2020, 12PM
         c.set(2020, 9, 25, 12, 00, 00);
         this.startPeriod = c.getTime();
+
+        // Default startPeriod: 25th November 2020, 12PM
         c.set(2020, 10, 25, 15, 00, 00);
         this.endPeriod = c.getTime();
-
-        System.out.println(startPeriod);
-        System.out.println(endPeriod);
     }
 
     public boolean validatePeriod(Date currentPeriod) {
@@ -30,27 +28,4 @@ public class Period {
         this.startPeriod = startPeriod;
         this.endPeriod = endPeriod;
     }
-
-
-    // DEBUG //
-    public static void main(String args[]){
-
-        Period period = new Period();
-
-        Calendar current = Calendar.getInstance();
-        Date currentPeriod = current.getTime();
-
-        System.out.println(current);
-        System.out.println(currentPeriod);
-
-        boolean inPeriod = period.validatePeriod(currentPeriod);
-
-        if(!inPeriod)
-            System.out.println("It's still not the registration period!");
-        else
-            System.out.println("You're in!");
-
-
-    }
-    
 }
