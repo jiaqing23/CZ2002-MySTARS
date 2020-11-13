e804e5d221f9a26954d3756880dea22755505188
wjwen5 <jwong097@e.ntu.edu.sg>
2020-11-13 16:17:38 +0800

    Add main in MySTARS.java

--- MySTARS.java
+++ MySTARS.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 
 public class MySTARS implements Serializable{
     private static String fileName = "mySTARS.txt";
-    private int mode;
+    private static int mode;
     private String username;
 
     private Period period = new Period();
@@ -18,16 +18,16 @@ public class MySTARS implements Serializable{
     private ArrayList<Student> students = new ArrayList<Student>();
     private ArrayList<Course> courses = new ArrayList<Course>();
 
-    public void addStudent(Student student){
+    public void addStudent(Student student) {
         students.add(student);
     }
 
-    public void addCourse(Course course){
+    public void addCourse(Course course) {
         courses.add(course);
     }
 
-    public boolean saveData(){
-        try{
+    public boolean saveData() {
+        try {
             FileOutputStream f = new FileOutputStream(new File(fileName));
             ObjectOutputStream o = new ObjectOutputStream(f);
 
@@ -36,17 +36,16 @@ public class MySTARS implements Serializable{
             o.close();
             f.close();
             return true;
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
         } catch (IOException e) {
             System.out.println("Error initializing stream");
-        } 
-        
+        }
+
         return false;
     }
 
-    public static MySTARS loadData(){
+    public static MySTARS loadData() {
         try {
             FileInputStream f = new FileInputStream(new File(fileName));
             ObjectInputStream o = new ObjectInputStream(f);
@@ -70,33 +69,86 @@ public class MySTARS implements Serializable{
         return null;
     }
 
-    public MySTARS(){
-        //TBD
+    public MySTARS() {
+        // TBD
     }
 
-    public boolean login(){
+    public boolean login() {
         Scanner sc = new Scanner(System.in);
         System.out.println("(1)Admin\t(2)Student");
         System.out.print("Mode: ");
-        this.mode = sc.nextInt(); 
+        this.mode = sc.nextInt();
         System.out.print("Username: ");
         this.username = sc.next();
         System.out.print("Password: ");
         String password = new String(System.console().readPassword());
-        return PasswordManager.validateAccount(username, password, mode==1);
+        return PasswordManager.validateAccount(username, password, mode == 1);
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
         MySTARS mainApp = loadData();
-        if(mainApp == null){
+        if (mainApp == null) {
             mainApp = new MySTARS();
         }
 
-        if(!mainApp.login()){
+        if (!mainApp.login()) {
             System.out.println("Invalid Credential!");
             return;
         }
-// Find user in arraylist
+        // Find user in arraylist
+        // Check date!!!!!!!!!!!!!!!
+        if (mode == 2) {
+            for (Student s : students) {
+                if(s.username = username){
+                    while(choice != 8){
+                        System.out.println("*************Welcome to MySTARS!*************");
+                        System.out.println("(1) Add Course ");
+                        System.out.println("(2) Drop Course");
+                        System.out.println("(3) Print Course Registered");
+                        System.out.println("(4) Check Course Vacancy");
+                        System.out.println("(5) Change Index Number of Course Registered");
+                        System.out.println("(6) Swap Index with Another Student");
+                        System.out.println("(7) Save Changes");
+                        System.out.println("(8) Quit");
+                        choice = sc.nextInt();
+
+                        switch(choice){
+                            case 1:
+                            //TBD
+                            break;
+                            case 2:
+                            //TBD
+                            break;
+                            case 3:
+                            //TBD
+                            break;
+                            case 4:
+                            //TBD
+                            break;
+                            case 5:
+                            //TBD
+                            break;
+                            case 6:
+                            //TBD
+                            break;
+                            case 7:
+                            if(saveData())
+                                System.out.println("Successfully Saved.");
+                            else System.out.println("Saving Failed.");
+                            break;
+                            case 8:
+                            
+                        }
+                    }
+                   
+
+                }
+                    
+            }
+        }
 //save data before logout
         
 
