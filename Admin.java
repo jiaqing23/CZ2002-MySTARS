a031e82d37211ce2bae49f9703de870078d49a68
wjwen5 <jwong097@e.ntu.edu.sg>
2020-11-16 00:05:32 +0800

    .

--- Admin.java
+++ Admin.java
@@ -15,9 +15,9 @@ public class Admin extends User implements Serializable{
         period.setPeriod(start, end);
     }
 
-    public void addStudent(String name, String username, String password, int maxAU, String gender, String nationality){
+    public void addStudent(String name, String username, String password, int maxAU, String gender, String nationality, String matricNumber){
         if(PasswordManager.addAccount(username, password)){
-            Student newStudent = new Student(name, username, maxAU, gender, nationality);
+            Student newStudent = new Student(maxAU, gender, nationality, matricNumber);
             mainApp.addStudent(newStudent);
             System.out.println("Student added!");
         }
