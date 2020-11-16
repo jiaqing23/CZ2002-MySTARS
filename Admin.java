785fc7e7eba8c24def47fe39dcbca62df1b3e62d
Jia Qing <jiaqing2303@gmail.com>
2020-11-16 15:18:00 +0800

    .

--- Admin.java
+++ Admin.java
@@ -1,26 +1,23 @@
+import java.io.Serializable;
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.Date;
+import java.util.Scanner;
 
-//inherited from User
-public class Admin extends User{
+public class Admin extends User implements Serializable{
     MySTARS mainApp;
 
-    //Constructor 
     public Admin(String name, String username){
         super(name, username);
     }
     
-    //Set the period of STARS
     public void editPeriod(Date start, Date end){
         Period period = mainApp.getPeriod();
         period.setPeriod(start, end);
     }
 
-    //Add a new student
     public void addStudent(String name, String username, String password, int maxAU, String gender, String nationality, String matricNumber){
         if(PasswordManager.addAccount(username, password)){
-            Student newStudent = new Student(name, username, maxAU, gender, nationality, matricNumber);
+            Student newStudent = new Student(maxAU, gender, nationality, matricNumber);
             mainApp.addStudent(newStudent);
             System.out.println("Student added!");
         }
@@ -29,23 +26,13 @@ public class Admin extends User{
         }
     }
 
-    //Add a new course
     public void addCourse(String school, String courseCode, String courseName, int numOfAU){
         Course course = new Course(school, courseCode, courseName, numOfAU);
-        mainApp.courses.add(course);
+        mainApp.addCourse(course);
     }
 
-    //To update the attributes of a course
-    public void updateCourse(String courseCode){
-        Course course;
-        for(Course c: mainApp.courses){
-            if(c.courseCode == courseCode){
-                course = c;
-                break;
-            }
-        }
-
-        System.out.println("Updating course " + courseCode);
+    public void updateCourse(Course course){
+        System.out.println("Updating course " + course.getCourseCode());
         System.out.println("(1) Update Course Code");
         System.out.println("(2) Update Course Name");
         System.out.println("(3) Update School");
@@ -75,9 +62,10 @@ public class Admin extends User{
                 break;
             case 4:
                 System.out.print("IndexNo to be added: ");
-                //Need read more attributes(indexno, classsize, XXXX)
                 tem = sc.nextLine();
-                course.addIndex(tem); 
+                System.out.print("IndexNo to be classSize: ");
+                int classSize = sc.nextInt();
+                course.addIndex(new Index(course, tem, classSize)); 
                 break;
             case 5:
                 System.out.print("IndexNo to be updated: ");
@@ -105,25 +93,22 @@ public class Admin extends User{
         }
     }
 
-    //To check number of vacancy available in an index
     public void checkVacancy(Index index){
         System.out.printf("Index %s have $d slots left.\n", index.getIndexNo(), index.getVacancy());
     }
 
-    //Print the name, nationaility and gender of students who registered for a certain course
     public void printByCourse(Course course){
         ArrayList<Index> indexes = course.getIndexes();
         for(Index index: indexes){
-            ArrayList<Student> students = index.getRegisteredStud();
+            ArrayList<Student> students = index.getReg();
             for(Student student: students){
                 System.out.printf("%s, %s, %s\n", student.getName(), student.getNationality(), student.getGender());
             }
         }
     }
 
-    //Print the name, nationaility and gender of students who registered for a certain index
     public void printByIndex(Index index){
-        ArrayList<Student> students = index.getRegisteredStud();
+        ArrayList<Student> students = index.getReg();
             for(Student student: students){
                 System.out.printf("%s, %s, %s\n", student.getName(), student.getNationality(), student.getGender());
         }
