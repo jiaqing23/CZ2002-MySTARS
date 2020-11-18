620a2e0776bdd5aefa26482741e0bd29ecfea985
yuanharnteo <bryson130191@gmail.com>
2020-11-19 00:27:31 +0800

    .

--- Period.java
+++ Period.java
@@ -2,40 +2,63 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 
-
+/**
+ * Represents the course registration Period of the University.
+ * Admin can access the system regardless of the registration period.
+ * Student can only access the system within the registration period.
+ */
 public class Period implements Serializable{
     Calendar calendar = Calendar.getInstance();
     private Date startPeriod;
     private Date endPeriod;
 
-    // CONSTRUCTOR
+    /**
+     * Creates a default registration Period.
+     * <p>
+     * Default start period: 25th October 2020, 12PM
+     * <p>
+     * Default end period: 25th November 2020, 12PM
+     */
     public Period(){
 
-        // Default startPeriod: 25th October 2020, 12PM
         // print format: Wed Nov 25 15:00:00 SGT 2020
         calendar.set(2020, 9, 25, 12, 00, 00);
         this.startPeriod = calendar.getTime();
 
-        // Default startPeriod: 25th November 2020, 12PM
         calendar.set(2020, 10, 25, 15, 00, 00);
         this.endPeriod = calendar.getTime();
     }
 
-    // SET AND GET METHODS
+    /**
+     * Method that gets the start time of the registration Period.
+     * @return Start time Date object of the calling Period object.
+     */
     public Date getStartPeriod() {
         return startPeriod;
     }
 
+    /**
+     * Method that gets the end time of the registration Period.
+     * @return End time Date object of the calling Period object.
+     */
     public Date getEndPeriod() {
         return endPeriod;
     }
 
+    /**
+     * Method that changes the start time and end time of the registration Period.
+     * @param startPeriod The new start time Date object of the calling Period object.
+     * @param endPeriod The new end time Date object of the calling Period object.
+     */
     public void setPeriod(Date startPeriod, Date endPeriod) {
         this.startPeriod = startPeriod;
         this.endPeriod = endPeriod;
     }
 
-    // CLASS METHODS
+    /**
+     * Method that validates the current time with the registration Period.
+     * @return True if current time is within the registration Period, else return false.
+     */
     public boolean validatePeriod() {
         Date currentDatetime = new Date();
         if(currentDatetime.compareTo(startPeriod) < 0 || currentDatetime.compareTo(endPeriod) > 0)
@@ -44,6 +67,9 @@ public class Period implements Serializable{
         return true;
     }
 
+    /**
+     * Method that prints the registration Period.
+     */
     public void printPeriod(){
         System.out.println("Start period: " + startPeriod);
         System.out.println("End period: " + endPeriod);
--- User.java
+++ User.java
@@ -1,5 +1,6 @@
 import java.io.Serializable;
 
+
 public class User implements Serializable {
     private String name;
     private String username;
