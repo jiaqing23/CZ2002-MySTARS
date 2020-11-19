8bfef46c2abd578df479eedaa6902397d114db11
yuanharnteo <bryson130191@gmail.com>
2020-11-19 20:55:05 +0800

    .

--- Notification.java
+++ Notification.java
@@ -5,8 +5,16 @@ import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+/**
+ * Represents an Email Notification that will be sent after a successful course registration.
+ */
 public class Notification {
 
+    /**
+     * Static method to initiate the process of sending an email notification.
+     * @param recepient Email account of a recepient.
+     * @param txt Text to include in the email notification.
+     */
     public static void sendMail(String recepient, String txt){
         System.out.println("Preparing to send email to " + recepient);
         Properties properties = new Properties();
@@ -39,14 +47,23 @@ public class Notification {
         }
     }
 
+    /**
+     * Static method to prepare the layout of the email.
+     * @param session Current mail session.
+     * @param myAccountEmail Host email account address.
+     * @param recepient Recepient's email account address.
+     * @param txt Email notification content.
+     * @return Email message model object.
+     */
     private static Message prepareMessage(Session session, String myAccountEmail, String recepient, String txt) {
         try {
            Message message = new MimeMessage(session);
            message.setFrom(new InternetAddress(myAccountEmail));
            message.setRecipient(Message.RecipientType.TO, new InternetAddress(recepient)); 
            message.setSubject("Course Registration"); 
-  
-           message.setText(txt); // set email content
+            
+           // set Email content
+           message.setText(txt);
            return message;
         } catch (Exception ex) {
            System.out.println("Email failed to send!");
@@ -55,6 +72,10 @@ public class Notification {
         return null;
     }
 
+    /**
+     * Driver program of Email notification.
+     * @param args Unused.
+     */
     public static void main(String[] args) {
         String msg = "Name:\t\tWONGJIAWEN\nMatric No.:\tacasdasda\n\nWe are pleased to inform you that you have been allocated the following course in coming semester:\nsadasdvavgzrhs";
 
